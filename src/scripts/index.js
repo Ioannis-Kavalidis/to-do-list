318ae55905c7c5441671081e3dc637c6e25569c4
paperjar <dreamtnem@gmail.com>
2020-04-23 17:12:33 +0200

    last commit

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,12 +1,38 @@
-const addToList = () => {
-  let userInput = document.querySelector("#userText").value;
-  let newLi = document.createElement("li");
-  let text = document.createTextNode(userInput);
-  newLi.appendChild(text);
-  document.querySelector(".result").appendChild(newLi);
-  document.querySelector("#userText").value = "";
+let colorGenerator = () => {
+  let characters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += characters[Math.floor(Math.random() * characters.length)];
+  }
+  color += "50"; // transparency
+  return color;
 };
 
+const addToList = () => {
+  let userInput = document.querySelector("#userText").value;
+  if (userInput == "") {
+    return alert("Please write something");
+  }
+  let newLi = document.createElement("li");
+  let NodeText = document.createTextNode(userInput);
+  newLi.appendChild(NodeText);
+  document.querySelector(".result").appendChild(newLi);
+  document.querySelector("#userText").value = "";
+  let list = document.querySelectorAll("li:nth-child(odd)");
+  list.forEach(({ style: item }) => {
+    item.backgroundColor = colorGenerator();
+  });
+  let span = document.createElement("span");
+  let text = document.createTextNode("bla");
+  span.className = "close";
+  span.appendChild(text);
+  let listAll = document.querySelectorAll("li");
+  listAll.forEach((item) => {
+    item.appendChild(span);
+  });
+};
+
+// .............................
 const addLastName = () => {
   let userInput2 = document.querySelector("#userText2").value;
   let newLi = document.createElement("li");
